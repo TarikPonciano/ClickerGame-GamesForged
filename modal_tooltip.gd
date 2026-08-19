276bb9164e2d80327ebaff17df2ea1f2a175e0eb
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-08-19 10:51:39 -0300

    Modal

--- modal_tooltip.gd
+++ modal_tooltip.gd
@@ -1,5 +1,8 @@
 extends Panel
 
+#var informacoes_visuais = {
+	#"Upgrade1": ["Patos Fofos", "A fofura dos patos, faz com que você clique mais motivado!", 100]
+#}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,4 +12,21 @@ func _ready() -> void:
 
 func _on_elemento_visual_mouse_entered(source: Control) -> void:
 	self.visible = true
-	print(source.name)
+	
+	if source.name == "Upgrade1":
+		$Titulo.text	 = "RGB no Computador"
+		$Descricao.text = "As luzes piscantes lhe motivam para clicar mais rápido e precisamente!"
+		$Preco.text = "$ 200.00"
+	elif source.name == "Upgrade2":
+		$Titulo.text	 = "Teclado Mecânico da Hello Kitty"
+		$Descricao.text = "O suave barulho das teclas fazem com que você possa clicar por mais tempo sem precisar de repouso!"
+		$Preco.text = "$ 500.00"
+	else:
+		self.visible = false
+	#$Titulo.text = informacoes_visuais[source.name][0]
+	#$Descricao.text = informacoes_visuais[source.name][1]
+	#$Preco.text = str(informacoes_visuais[source.name][2])
+
+
+func _on_elemento_visual_mouse_exited() -> void:
+	self.visible = false
