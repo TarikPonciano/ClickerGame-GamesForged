53004933c3e6b15e2b7549dde4c2b574ac7bd5ab
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-09-02 09:48:44 -0300

    Update save_load.gd

--- save_load.gd
+++ save_load.gd
@@ -1,1 +1,24 @@
 extends Node
+
+const SAVE_PATH = "user://saveclicker.json"
+const LS_KEY = "savegame"
+
+func salvar_jogo():
+	var dados_jogo = {
+		"currency": Global.currency,
+		"ganho_passivo": Global.ganho_passivo,
+		"upg_click_nivel": Global.upg_click_nivel,
+		"upg_click_custo": Global.upg_click_custo, 
+		"upg_gerador_1_nivel": Global.upg_gerador_1_nivel,
+		"upg_gerador_1_poder": Global.upg_gerador_1_poder,
+		"upg_gerador_1_custo": Global.upg_gerador_1_custo,
+		"upgrade_1_comprado": Global.upgrade_1_comprado,
+		"upgrade_2_comprado":  Global.upgrade_2_comprado
+	} 
+	
+	if OS.has_feature("web"):
+		pass
+	else:
+		pass
+	
+	
